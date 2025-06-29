1ffbc75df1af43f55d3710d96532496bbc46c711
slide-t <slideteckacademy@gmail.com>
2025-06-29 05:07:26 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,32 @@
 /* === service-worker.js === */
 
-const CACHE_NAME = 'js-tutorial-v1'; const urlsToCache = [ '/', '/index.html', '/styles.css', '/script.js', '/manifest.json', '/favicon.ico', '/lesson1.html', '/lesson2.html', '/lesson3.html', // Add all lesson pages here... ];
+const CACHE_NAME = 'js-tutorial-v1'; const urlsToCache = [
+  '/', '/index.html', 
+  '/styles.css', 
+  '/script.js', 
+  '/manifest.json', 
+  '/favicon.ico',
+  '/lesson1.html',
+  '/lesson2.html',
+  '/lesson3.html',
+  '/lesson4.html',
+  '/lesson5.html',
+  '/lesson6.html',
+  '/lesson7.html',
+  '/lesson8.html',
+  '/lesson9.html',
+  '/lesson10.html',
+  '/lesson11.html',
+  '/lesson12.html',
+   '/lesson13.html',
+  '/lesson14.html',
+  '/lesson15.html',
+  '/lesson16.html',
+  '/lesson17.html',
+  '/lesson18.html',
+  
+  // Add all lesson pages here...
+  ];
 
 self.addEventListener('install', (event) => { event.waitUntil( caches.open(CACHE_NAME).then((cache) => { return cache.addAll(urlsToCache); }) ); });
 
